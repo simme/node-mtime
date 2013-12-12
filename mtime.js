700d83aea6972f437dc96599c272c7eaa9f72cd4
Simon Ljungberg <hi@iamsim.me>
2013-12-12 10:13:24 +0100

    Initial commit.

---
+++ mtime.js
@@ -0,0 +1,98 @@
+//
+// # mtime
+//
+// Find last modification date of the given path. Can be recursive to find the
+// date of the last modified file inside a directory.
+//
+// * **path**, path to check.
+// * **recursive**, check all files in the given path if a dir. Return latest.
+// * **callback**, if callback is provided the operation is done async.
+//   Sync version will throw on error.
+//
+
+/* jslint node: true */
+"use strict";
+
+var fs   = require('fs');
+var join = require('path').join;
+
+module.exports = function (path, recursive, fn) {
+  var sync = true;
+  if (arguments.length === 2) {
+    if (typeof recursive === 'function') {
+      sync = false;
+      fn = recursive;
+      recursive = false;
+    }
+  }
+  else if (arguments.length === 3) {
+    sync = false;
+  }
+
+  return sync ? mtimeSync(path, recursive) : mtimeAsync(path, recursive, fn);
+};
+
+function mtimeSync(path, recursive) {
+  var exists = fs.existsSync(path);
+  if (!exists) {
+    throw new Error('Path: ' + path + ' not found');
+  }
+
+  var stat = fs.statSync(path);
+  if (stat.isFile() || stat.isDirectory() && !recursive) {
+    return stat.mtime.getTime();
+  }
+
+  var files = fs.readdirSync(path);
+  var mtime = stat.mtime.getTime();
+  files.forEach(function (file) {
+    var time = mtimeSync(join(path, file), true);
+    mtime = Math.max(mtime, time);
+  });
+
+  return mtime;
+}
+
+function mtimeAsync(path, recursive, fn) {
+  fs.exists(path, function (exists) {
+    if (!exists) {
+      fn(new Error('Path: ' + path + ' not found'));
+      return;
+    }
+
+    fs.stat(path, function (err, stat) {
+      if (err) {
+        fn(err);
+        return;
+      }
+
+      if (stat.isFile() || stat.isDirectory() && !recursive) {
+        fn(null, stat.mtime.getTime());
+        return;
+      }
+
+      fs.readdir(path, function (err, files) {
+        if (err) {
+          fn(err);
+          return;
+        }
+
+        var mtime = stat.mtime.getTime();
+        function recurse(file) {
+          mtimeAsync(file, true, function (err, t) {
+            mtime = Math.max(mtime, t);
+            var f = files.pop();
+            if (!f) {
+              fn(null, mtime);
+              return;
+            }
+            recurse(join(path, f));
+          });
+        }
+
+        recurse(join(path, files.pop()));
+      });
+    });
+  });
+}
+
---
+++ test/test.js
@@ -0,0 +1,87 @@
+var assert = require('assert');
+var mtime  = require('..');
+var touch  = require('touch');
+var mkdirp = require('mkdirp');
+var fs     = require('fs');
+var rimraf = require('rimraf');
+
+function m(file) {
+  return (fs.statSync(file)).mtime.getTime();
+}
+
+var ms = Date.now();
+function nd() {
+  ms = ms += 360000;
+  var d = new Date(ms);
+  return d;
+}
+
+suite('mtime', function () {
+  // Create some random files in /tmp
+  setup(function (done) {
+    mkdirp.sync('/tmp/_mtime', 0777);
+    mkdirp.sync('/tmp/_mtime/foo', 0777);
+    mkdirp.sync('/tmp/_mtime/bar/baz', 0777);
+    touch.sync('/tmp/_mtime/a', { mtime: nd()});
+    touch.sync('/tmp/_mtime/b', { mtime: nd()});
+    touch.sync('/tmp/_mtime/c', { mtime: nd()});
+    touch.sync('/tmp/_mtime/foo/1', { mtime: nd()});
+    touch.sync('/tmp/_mtime/foo/2', { mtime: nd()});
+    touch.sync('/tmp/_mtime/foo/3', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/1', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/2', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/3', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/baz/1', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/baz/2', { mtime: nd()});
+    touch.sync('/tmp/_mtime/bar/baz/3', { mtime: nd()});
+    done();
+  });
+
+  // Delete test files
+  teardown(function () {
+    try {
+      //rimraf.sync('/tmp/_mtime');
+    }
+    catch (e) {
+      console.log('\t**Failed to delete test files.**');
+      console.log('\t%s', e);
+    }
+  });
+
+  test('Sync mtime works', function () {
+    var t = mtime('/tmp/_mtime');
+    var x = m('/tmp/_mtime');
+    assert.equal(t, x);
+
+    t = mtime('/tmp/_mtime/foo/1');
+    x = m('/tmp/_mtime/foo/1');
+    assert.equal(t, x);
+  });
+
+  test('Recursive sync mtime works', function () {
+    var t = mtime('/tmp/_mtime', true);
+    var x = m('/tmp/_mtime/bar/baz/3');
+
+    t = mtime('/tmp/_mtime/foo', true);
+    x = m('/tmp/_mtime/foo/3');
+  });
+
+  test('Async mtime works', function (done) {
+    mtime('/tmp/_mtime/a', function (err, t) {
+      assert(!err);
+      var x = m('/tmp/_mtime/a');
+      assert.equal(t, x);
+      done();
+    });
+  });
+
+  test('Recursive async mtime works', function (done) {
+    mtime('/tmp/_mtime/', true, function (err, t) {
+      assert(!err);
+      var x = m('/tmp/_mtime/bar/baz/3');
+      assert.equal(t, x);
+      done();
+    });
+  });
+});
+
